dechamps/WinIPBroadcast

commit 3aa98d58ec1b61cec39d2c0c5c70598d24dca927

Stop using source address as a filter; use TTL instead.
In order to avoid relay loops, the previous code only relayed the packet
if it had a source address that matched the default source address for
packets sent to the global broadcast address. If the packet had a
different source address, it would assume it came from WinIPBroadcast
itself and ignore it.

This works fine as long as the sending process leaves the socket unbound
to any source address while sending broadcast packets. However, if the
process explicitly binds the socket to a specific source address, and
that source address doesn't match the default source address, then
WinIPBroadcast will not work.

This commit fixes the issue by unconditionally relaying packets no
matter what their source address is. To avoid relay loops, we use a
different approach which consists in setting the TTL of outgoing packets
to just 1. When these packets come back in they will be dropped because
of their TTL. This is not a perfect solution, but it is arguably
superior to the previous approach.

WinIPBroadcast.c
@@ -98,26 +98,6 @@ void initListenSocket(void)
 		socketError(TEXT("bind"), TRUE);
 }
 
-ULONG broadcastRouteAddress(SOCKET socket)
-{
-	sockaddr_gen ourAddress;
-	sockaddr_gen routeAddress;
-	DWORD len;
-	
-	ourAddress.Address.sa_family = AF_INET;
-	ourAddress.AddressIn.sin_addr.s_addr = broadcastAddress;
-
-	if (WSAIoctl(socket, SIO_ROUTING_INTERFACE_QUERY, &ourAddress, sizeof(ourAddress), &routeAddress, sizeof(routeAddress), &len, NULL, NULL) == SOCKET_ERROR)
-	{
-		if (WSAGetLastError() == WSAENETUNREACH || WSAGetLastError() == WSAEHOSTUNREACH || WSAGetLastError() == WSAENETDOWN)
-			return 0;
-	
-		socketError(TEXT("WSAIoctl(SIO_ROUTING_INTERFACE_QUERY)"), TRUE);
-	}
-		
-	return routeAddress.AddressIn.sin_addr.s_addr;
-}
-
 DWORD getBroadcastPacket(char *buffer, size_t size, ULONG *srcAddress)
 {
 	DWORD len;
@@ -129,21 +109,20 @@ DWORD getBroadcastPacket(char *buffer, size_t size, ULONG *srcAddress)
 	
 	flags = 0;
 	
-	do
+	while (TRUE)
 	{
 		if (WSARecv(listenSocket, &wsaBuffer, 1, &len, &flags, NULL, NULL) != 0)
 			socketError(TEXT("WSARecvMsg"), TRUE);
 		
 		if (len < IP_HEADER_SIZE + UDP_HEADER_SIZE)
 			continue;
-			
+
+		if (*((ULONG *)&buffer[IP_DSTADDR_POS]) != broadcastAddress)
+			continue;
+
 		*srcAddress = *((ULONG *) &buffer[IP_SRCADDR_POS]);
+		break;
 	}
-	while (
-		*((ULONG *) &buffer[IP_DSTADDR_POS]) != broadcastAddress
-		||
-		broadcastRouteAddress(listenSocket) != *srcAddress
-	);
 	
 	return len;
 }
@@ -248,6 +227,22 @@ void sendBroadcast(ULONG srcAddress, char *payload, uint16_t payloadSize)
 	if (setsockopt(socket, SOL_SOCKET, SO_BROADCAST, (char *)&broadcastOpt, sizeof(broadcastOpt)) == SOCKET_ERROR)
 	{
 		socketError(TEXT("setsockopt(SO_BROADCAST)"), FALSE);
+		closesocket(socket);
+		return;
+	}
+
+	// Any packet we send here will also appear in the input. This can result in an infinite relay loop.
+	// To avoid this, we explicitly set the minimum possible TTL. Incoming packets with this TTL will not be relayed.
+	//
+	// TODO: this is arguably not the best way to solve this problem, because it could also result in packets
+	// getting dropped along the route. Theoretically it should make no difference because broadcast packets can't be
+	// routed anyway (TTL or not), but one can imagine special setups where that might matter. There is also the
+	// problem that if the sending process has the same idea and also sets the TTL to 1, its packets won't be relayed.
+	// A better way would be to implement some kind of content-based deduplication, but that's way more complex.
+	DWORD ttl = 1;
+	if (setsockopt(socket, IPPROTO_IP, IP_TTL, (char*)&ttl, sizeof(ttl)) == SOCKET_ERROR)
+	{
+		socketError(TEXT("setsockopt(IP_TTL)"), FALSE);
 		closesocket(socket);
 		return;
 	}

commit 6c5b669d06f6ee0a0507e34f776305524ea0ff1f

Fix a bug where WinIPBroadcast would relay packets from the outside.
The regression was introduced in 3aa98d5. This commit fixes the bug
by making sure the source address of the incoming packet appears in
at least one broadcast route.

WinIPBroadcast.c
@@ -98,6 +98,51 @@ void initListenSocket(void)
 		socketError(TEXT("bind"), TRUE);
 }
 
+void getForwardTable()
+{
+	int result;
+
+retry:
+
+	result = GetIpForwardTable(forwardTable, &forwardTableSize, FALSE);
+
+	switch (result)
+	{
+	case NO_ERROR:
+		break;
+
+	case ERROR_INSUFFICIENT_BUFFER:
+		free(forwardTable);
+		forwardTable = malloc(forwardTableSize);
+		goto retry;
+
+	default:
+		fwprintf(stderr, TEXT("error: GetIpForwardTable failed with error code %d: %s"), result, errorString(result));
+		quit();
+	}
+}
+
+BOOL findLocalAddressInBroadcastRoutes(const ULONG *srcAddress) {
+	for (DWORD i = 0; i < forwardTable->dwNumEntries; i++)
+	{
+		PMIB_IPFORWARDROW row = &forwardTable->table[i];
+
+		if (row->dwForwardDest != broadcastAddress)
+			continue;
+
+		if (row->dwForwardMask != ULONG_MAX)
+			continue;
+
+		if (row->dwForwardType != MIB_IPROUTE_TYPE_DIRECT)
+			continue;
+
+		if (row->dwForwardNextHop == *srcAddress)
+			return TRUE;
+	}
+
+	return FALSE;
+}
+
 DWORD getBroadcastPacket(char *buffer, size_t size, ULONG *srcAddress)
 {
 	DWORD len;
@@ -108,6 +153,8 @@ DWORD getBroadcastPacket(char *buffer, size_t size, ULONG *srcAddress)
 	wsaBuffer.len = size;
 	
 	flags = 0;
+
+	getForwardTable();
 	
 	while (TRUE)
 	{
@@ -121,34 +168,15 @@ DWORD getBroadcastPacket(char *buffer, size_t size, ULONG *srcAddress)
 			continue;
 
 		*srcAddress = *((ULONG *) &buffer[IP_SRCADDR_POS]);
+
+		// We will also receive broadcast packets coming from the outside. Filter those out.
+		if (!findLocalAddressInBroadcastRoutes(srcAddress))
+			continue;
+
 		break;
 	}
-	
+
 	return len;
-}
-
-void getForwardTable()
-{
-	int result;
-	
-	retry:
-	
-	result = GetIpForwardTable(forwardTable, &forwardTableSize, FALSE);
-
-	switch (result)
-	{
-		case NO_ERROR:
-			break;
-			
-		case ERROR_INSUFFICIENT_BUFFER:
-			free(forwardTable);
-			forwardTable = malloc(forwardTableSize);
-			goto retry;
-			
-		default:
-			fwprintf(stderr, TEXT("error: GetIpForwardTable failed with error code %d: %s"), result, errorString(result));
-			quit();
-	}
 }
 
 void computeUdpChecksum(char *payload, uint16_t payloadSize, DWORD srcAddress, DWORD dstAddress)
@@ -267,8 +295,6 @@ void relayBroadcast(char *payload, uint16_t payloadSize, ULONG srcAddress)
 {
 	DWORD i;
 	PMIB_IPFORWARDROW row;
-
-	getForwardTable();
 	
 	for (i = 0; i < forwardTable->dwNumEntries; i++)
 	{